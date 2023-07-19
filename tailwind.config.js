4fb737907c7d90962247fe087609c25bd3009069
revealdone <muhamadwahidromdoni@gmail.com>
2023-07-19 20:05:18 +0700

    Pesan commit Anda di sini

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         primary: "#14b8a6",
         secondary: "#64748b",
         dark: "#0f172a",
+        red: "#E38B29",
       },
       screens: {
         "2xl": "1320px",
