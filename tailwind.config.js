87704ecc8247c9df8bd1def55bae55fe3aa51d84
revealdone <muhamadwahidromdoni@gmail.com>
2023-07-19 17:53:28 +0700

    aa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
-    container:{
+    container: {
       center: true,
-      padding: '16px'
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6' ,
-        secondary: '#64748b',
-        dark: '#0f172a',
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
       screens: {
-        '2xl': '1320px',
-      }
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
-
+};
